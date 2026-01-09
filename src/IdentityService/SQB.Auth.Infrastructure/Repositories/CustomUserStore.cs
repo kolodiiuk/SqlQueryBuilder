d7fe09f382d628c7f53c3127036aa94f7a6b2d0e
kolodiiuk <nkolodeyuk@gmail.com>
2026-01-09 19:04:53 +0200

    feat(identity): impl user passwordd store methods

--- src/IdentityService/SQB.Auth.Infrastructure/Repositories/CustomUserStore.cs
+++ src/IdentityService/SQB.Auth.Infrastructure/Repositories/CustomUserStore.cs
@@ -63,13 +63,24 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
                            update
                                   users
                            set
-                                  user_name = @user_name
+                                  user_name = @user_name,
+                                  concurrency_stamp = @concurrency_stamp
                            where
                                   id = @user_id
+                                  and concurrency_stamp = @concurrency_stamp
                            """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        await conn.ExecuteAsync(sql, new { user_name = userName, user_id = user.Id });
+        await conn.ExecuteAsync(sql,
+            new
+            {
+                user_name = userName,
+                user_id = user.Id,
+                concurrency_stamp = user.ConcurrencyStamp,
+                new_concurrency_stamp = newConcurrencyStamp
+            });
+        //todo: error handling for concurrency
     }
 
     public async Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
@@ -97,25 +108,137 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
                            update
                                   users
                            set
-                                  normalized_name = @normalized_name
+                                  normalized_name = @normalized_name,
+                                  concurrency_stamp = @new_concurrency_stamp
                            where
                                   id = @user_id
+                                  and concurrency_stamp = @concurrency_stamp
                            """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        await conn.ExecuteAsync(sql, new { normalized_name = normalizedName, user_id = user.Id });
+        await conn.ExecuteAsync(sql, new
+        {
+            normalized_name = normalizedName,
+            user_id = user.Id,
+            concurrency_stamp = user.ConcurrencyStamp,
+            new_concurrency_stamp = newConcurrencyStamp
+        });
+        //todo: error handling for concurrency
     }
 
     public async Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        throw new NotImplementedException();
+        const string sql = """
+                           insert into users
+                           (id, user_name, normalized_user_name, email, normalized_email,
+                            email_confirmed, password_hash, security_stamp, concurrency_stamp,
+                            phone_number, phone_number_confirmed, two_factor_enabled, lockout_end,
+                            lockout_enabled, access_failed_count, created_at, updated_at)
+                           values (@id, @user_name, @normalized_user_name, @email, @normalized_email,
+                                   @email_confirmed, @password_hash, @security_stamp, @concurrency_stamp,
+                                   @phone_number, @phone_number_confirmed, @two_factor_enabled, @lockout_end,
+                                   @lockout_enabled, @access_failed_count, @created_at, @updated_at)
+                           """;
+        var guid = Guid.NewGuid();
+        try
+        {
+            await using var conn = Open();
+            cancellationToken.ThrowIfCancellationRequested();
+            var rows = await conn.ExecuteAsync(new CommandDefinition(sql, new
+                {
+                    id = guid,
+                    user_name = user.UserName,
+                    normalized_user_name = user.NormalizedUserName,
+                    email = user.Email,
+                    normalized_email = user.NormalizedEmail,
+                    email_confirmed = user.EmailConfirmed,
+                    password_hash = user.PasswordHash,
+                    security_stamp = user.SecurityStamp,
+                    concurrency_stamp = user.ConcurrencyStamp,
+                    phone_number = user.PhoneNumber,
+                    phone_number_confirmed = user.PhoneNumberConfirmed,
+                    two_factor_enabled = user.TwoFactorEnabled,
+                    lockout_end = user.LockoutEnd,
+                    lockout_enabled = user.LockoutEnabled,
+                    access_failed_count = user.AccessFailedCount,
+                    created_at = user.CreatedAt,
+                    updated_at = user.UpdatedAt
+                },
+                cancellationToken: cancellationToken));
+
+            return IdentityResult.Success;
+        }
+        catch (PostgresException e) when (e.SqlState == PostgresErrorCodes.UniqueViolation)
+        {
+            return IdentityResult.Failed(new IdentityError()
+            {
+                Code = "UniqueViolation",
+                Description = "User is already in db"
+            });
+        }
     }
 
     public async Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        throw new NotImplementedException();
+        const string sql = """
+                           update users
+                           set 
+                               user_name = @user_name,
+                               normalized_user_name = @normalized_user_name,
+                               email = @email,
+                               normalized_email = @normalized_email,
+                               password_hash = @password_hash,
+                               security_stamp = @security_stamp,
+                               concurrency_stamp = @new_concurrency_stamp,
+                               phone_number = @phone_number, 
+                               phone_number_confirmed = @phone_number_confirmed,  
+                               two_factor_enabled = @two_factor_enabled, 
+                               lockout_end = @lockout_end,
+                               lockout_enabled = @lockout_enabled, 
+                               access_failed_count = @access_failed_count,
+                               updated_at = @updated_at
+                           where 
+                               id = @id 
+                               and concurrency_stamp = @old_stamp;
+                           """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
+        var now = DateTime.UtcNow;
+        await using var conn = Open();
+        var rows = await conn.ExecuteAsync(new CommandDefinition(sql,
+            new
+            {
+                id = user.Id,
+                old_stamp = user.ConcurrencyStamp,
+                new_concurrency_stamp = newConcurrencyStamp,
+                user_name = user.UserName,
+                normalized_user_name = user.NormalizedUserName,
+                email = user.Email,
+                normalized_email = user.NormalizedEmail,
+                password_hash = user.PasswordHash,
+                security_stamp = user.SecurityStamp,
+                phone_number = user.PhoneNumber,
+                phone_number_confirmed = user.PhoneNumberConfirmed,
+                two_factor_enabled = user.TwoFactorEnabled,
+                lockout_enabled = user.LockoutEnabled,
+                lockout_end = user.LockoutEnd,
+                access_failed_count = user.AccessFailedCount,
+                updated_at = user.UpdatedAt
+            }, cancellationToken: cancellationToken));
+        if (rows != 0)
+        {
+            user.ConcurrencyStamp = newConcurrencyStamp;
+
+            return IdentityResult.Success;
+        }
+
+        return IdentityResult.Failed(new IdentityError()
+        {
+            Code = "ConcurrencyFailure",
+            Description = "Concurrent update failure"
+        });
     }
 
     public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
@@ -124,31 +247,41 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
         const string sql = """
                            delete from users
                            where
-                               id = @user_id
+                               id = @user_id and concurrency_stamp = @concurrency_stamp
                            """;
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        var rows = await conn.ExecuteAsync(sql, new { user_id = user.Id });
+        var rows = await conn.ExecuteAsync(new CommandDefinition(sql,
+            new { user_id = user.Id, concurrency_stamp = user.ConcurrencyStamp },
+            cancellationToken: cancellationToken));
+        if (rows != 0)
+        {
+            return IdentityResult.Success;
+        }
 
-        throw new NotImplementedException();
+        return IdentityResult.Failed(new IdentityError()
+        {
+            Code = "ConcurrencyFailure",
+            Description = "Concurrency stamps did not match or user is not in db"
+        });
     }
 
     public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
         const string getUserSql = """
-                                  select
-                                        id,
-                                        user_name,
-                                        email,
-                                        password_hash,
-                                        created_at,
-                                        updated_at
-                                   from 
-                                        users 
-                                   where 
-                                        id = @id
-                           """;
+                                         select
+                                               id,
+                                               user_name,
+                                               email,
+                                               password_hash,
+                                               created_at,
+                                               updated_at
+                                          from 
+                                               users 
+                                          where 
+                                               id = @id
+                                  """;
         const string getUserRoleSql = """
                                       select
                                             r.id,
@@ -212,13 +345,24 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
                            update
                                 users
                            set
-                                password_hash = @password_hash
+                                password_hash = @password_hash,
+                                concurrency_stamp = @new_concurrency_stamp
                            where
                                 id = @user_id
+                                and concurrency_stamp = @concurrency_stamp
                            """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        await conn.ExecuteAsync(sql, new { password_hash = passwordHash, user_id = user.Id });
+        await conn.ExecuteAsync(sql,
+            new
+            {
+                password_hash = passwordHash,
+                user_id = user.Id,
+                concurrency_stamp = user.ConcurrencyStamp,
+                new_concurrency_stamp = newConcurrencyStamp
+            });
+        //todo: error handling for concurrency
     }
 
     public async Task<string> GetPasswordHashAsync(User user, CancellationToken cancellationToken)
@@ -328,16 +472,27 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
     {
         cancellationToken.ThrowIfCancellationRequested();
         const string sql = """
-                  update 
-                        users
-                  set
-                      email = @email,
-                  where
-                      id = @user_id
-                  """;
+                           update 
+                                 users
+                           set
+                               email = @email,
+                               concurrency_stamp = @new_concurrency_stamp
+                           where
+                               id = @user_id
+                               and concurrency_stamp = @concurrency_stamp
+                           """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        await conn.ExecuteAsync(sql, new { email = email, user_id = user.Id });
+        await conn.ExecuteAsync(sql,
+            new
+            {
+                email = email,
+                user_id = user.Id,
+                concurrency_stamp = user.ConcurrencyStamp,
+                new_concurrency_stamp = newConcurrencyStamp
+            });
+        //todo: error handling for concurrency
     }
 
     public async Task<string> GetEmailAsync(User user, CancellationToken cancellationToken)
@@ -380,16 +535,26 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
     {
         cancellationToken.ThrowIfCancellationRequested();
         const string sql = """
-                  update 
-                        users
-                  set
-                      email_confirmed = @confirmed
-                  where
-                      id = @user_id
-                  """;
+                           update 
+                                 users
+                           set
+                               email_confirmed = @confirmed,
+                               concurrency_stamp = @new_concurrency_stamp
+                           where
+                               id = @user_id
+                               and concurrency_stamp = @concurrency_stamp
+                           """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        await conn.ExecuteAsync(sql, new { confirmed = confirmed, user_id = user.Id });
+        await conn.ExecuteAsync(sql, new
+        {
+            confirmed = confirmed,
+            user_id = user.Id,
+            concurrrency_stamp = user.ConcurrencyStamp,
+            new_concurrency_stamp = newConcurrencyStamp
+        });
+        //todo: error handling for concurrency
     }
 
     public async Task<User> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
@@ -439,13 +604,23 @@ public class CustomUserStore : IUserPasswordStore<User>, IUserRoleStore<User>, I
                   update 
                         users
                   set
-                        normalized_email = @normalized_email
+                        normalized_email = @normalized_email,
+                        concurrency_stamp = @new_concurrency_stamp
                   where
                       id = @user_id
+                      and concurrency_stamp = @concurrency_stamp
                   """;
+        var newConcurrencyStamp = Guid.NewGuid().ToString();
         await using var conn = Open();
         cancellationToken.ThrowIfCancellationRequested();
-        await conn.ExecuteAsync(sql, new { normalized_email = normalizedEmail, user_id = user.Id });
+        await conn.ExecuteAsync(sql, new
+        {
+            normalized_email = normalizedEmail,
+            user_id = user.Id,
+            concurrency_stamp = user.ConcurrencyStamp,
+            new_concurrency_stamp = newConcurrencyStamp
+        });
+        //todo: error handling for concurrency
     }
 
     private NpgsqlConnection Open()
